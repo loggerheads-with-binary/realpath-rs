78d71139664ffeb851649f467d19823d375efef8
Aniruddh Radhakrishnan <aniruddh.iitb.92@gmail.com>
2023-03-30 05:32:17 +0530

    Documentation change

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A cross-platform Rust equivalent of python3's `os.path.realpath`
 ## Usage of program 
 
 ```rust 
-use realpath_rs::realpath; 
+use realpath::realpath; 
 use std::path::PathBuf; 
 
 let src = PathBuf::from("Cargo.toml");
--- src/windows_realpath.rs
+++ src/windows_realpath.rs
@@ -1,9 +1,10 @@
-use std::path::{Path, PathBuf};
+use std::path::{PathBuf};
 
 extern crate path_absolutize;
 use path_absolutize::Absolutize;
 
-fn loop_okay(path: &mut PathBuf) {
+#[no_mangle]
+fn step_by_step_canonicalize(path: &mut PathBuf) {
 
     let mut components = path.components().collect::<Vec<_>>();
     let mut head = path.clone();
@@ -40,6 +41,6 @@ pub fn realpath(path: &PathBuf) -> Result<PathBuf, std::io::Error> {
     
     let mut path = path.absolutize()?.to_path_buf();
 
-    loop_okay(&mut path);
+    step_by_step_canonicalize(&mut path);
     Ok(path)
 }
