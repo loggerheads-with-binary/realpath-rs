6bdfc9db6c40a1b49beeedbb3beb72785142c74e
Aniruddh Radhakrishnan <aniruddh.iitb.92@gmail.com>
2023-03-30 12:35:56 +0530

    Added documentation for Cargo

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
+/// ## Usage
+///
+/// ```rust 
+/// use realpath::realpath; 
+/// use std::path::PathBuf; 
+
+/// let src = PathBuf::from("Cargo.toml");
+/// let dest : PathBuf = realpath(&src)?; 
+/// println!("{} -> {}", src, dest); 
+/// ```
+
+
 mod util; 
 pub use util::realpath; 
